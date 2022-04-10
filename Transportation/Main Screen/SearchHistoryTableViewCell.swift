1232d8a165a130cef067ce698dff1932257af96a
Amira <amira@gmail.com>
2022-04-10 14:03:56 +0100

    add borders on recent search table

--- Transportation/Main Screen/SearchHistoryTableViewCell.swift
+++ Transportation/Main Screen/SearchHistoryTableViewCell.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class SearchHistoryTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var containerStackView: UIStackView!
     @IBOutlet weak var fromHistorySearchLabel: UILabel!
     @IBOutlet weak var toHistorySearchLabel: UILabel!
 
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        containerStackView.layer.borderWidth = 1
+        containerStackView.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
     func setupHistoryCell(from: String, to: String){
         fromHistorySearchLabel.text = from
         toHistorySearchLabel.text = to
